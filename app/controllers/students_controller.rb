18b463951e45e029b7dc796cf6f96e2fd1d5f639
Charles Lai <charlesangeloplai@gmail.com>
2021-01-14 14:26:43 -0500

    commit tree f2b89bbbfd92eee26ccf1d92d686151bdc017a9f

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,28 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(params[:id])
+    find_student
   end
+  
+  def new
+    @student = Student.new
+  end
+  
+  def create
+    @movie = Student.new(student_params)
+    if @movie.save
+      redirect_to students_path
+    else
+      render :new
+    end
+  end
+  
+  private
+    def find_student
+      @student = Student.find(params[:id])
+    end
 
+    def student_params
+      params.require(:student).permit(:first_name, :last_name)
+    end
 end
